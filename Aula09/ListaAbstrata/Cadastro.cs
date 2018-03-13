9e1eaef065b0f91d63cdb795a15299f272a19a81
Aluno <Aluno@Digital.net>
2018-03-12 22:16:04 -0300

    isso ai

---
+++ Aula09/ListaAbstrata/Cadastro.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaAbstrata
+{
+    class Cadastro
+    {
+        private int tamanho;
+        private ListaObj lista;
+
+        public Cadastro(int tamanho)
+        {
+            lista = new ListaObj(this.tamanho);
+            this.tamanho = tamanho;
+        }
+             
+
+        public void AdicionaAluno(Aluno a)
+        {
+            lista.Adicionar(a);
+        }
+
+        public void ExibeTodos()
+        {
+            for(int i=0; i<tamanho; i++)
+            {
+                Console.WriteLine(lista.BuscarPeloI(i).ToString());
+            }
+        }
+
+        public Aluno BuscaRA(string ra)
+        {
+            for(int i=0; i<tamanho; i++)
+            {
+                Aluno a = (Aluno)lista.BuscarPeloI(i);
+                if (a.Ra == ra) return a;
+            }
+            return null;
+        }
+
+    }
+
+}
---
+++ Aula09/ListaAbstrata/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaAbstrata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno euJovem = new AlunoFundamental(10, 10, 10, 10, "Mariana", "19");
+            Aluno euAgora = new AlunoGraduacao(8, 9, "Mariana", "40006");
+            Aluno euVelha = new AlunoPos(10, 10, 2, "Mariana", "314159265");
+
+            euJovem.CalculaMedia();
+            euAgora.CalculaMedia();
+            euVelha.CalculaMedia();
+
+            Console.WriteLine(euJovem.ToString());
+            Console.WriteLine(euAgora.ToString());
+            Console.WriteLine(euVelha.ToString());
+
+            Cadastro cadastro = new Cadastro(3);
+            cadastro.AdicionaAluno(euJovem);
+            cadastro.AdicionaAluno(euAgora);
+            cadastro.AdicionaAluno(euVelha);
+
+            cadastro.ExibeTodos();
+
+            Console.ReadKey();
+
+        }
+    }
+}
